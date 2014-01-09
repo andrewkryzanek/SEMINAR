d839e6e2cd24844df2cacc8efa8be946768fa3ef
Alan Arnholt <arnholtat@appstate.edu>
2014-01-09 14:05:05 -0500

    staging all files

--- Alan/summaries/RmakeFile.R
+++ Alan/summaries/RmakeFile.R
@@ -7,5 +7,6 @@ require(markdown)
 
 # Create .md, .html, and .pdf files
 knit("GIT_LAB1.Rmd")
+# Really do not need this one
 # markdownToHTML('GIT_LAB1.md', 'GIT_LAB1.html', options=c("use_xhml"))
 system("pandoc -s GIT_LAB1.html -o GIT_LAB1.pdf -V geometry:margin=1in")
